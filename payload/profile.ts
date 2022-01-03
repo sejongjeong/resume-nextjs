cb596ec39fea8ae6ea68d87f9a1fa2e253f39cd7
Sejong Jeong <wjdtpwhd@gmail.com>
2022-01-03 09:29:36 +0900

    feat: Modified profile

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,7 +1,7 @@
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone, faBold } from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { faGithub, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import image from '../asset/_sample.jpg';
+import image from '../asset/sejong.jpg';
 import { IProfile } from '../component/profile/IProfile';
 
 const profile: IProfile.Payload = {
@@ -10,13 +10,13 @@ const profile: IProfile.Payload = {
   // image: 'https://resume.yowu.dev/static/image/profile_2019.png',
   image,
   name: {
-    title: 'Lorem ipsum',
-    small: '(LI)',
+    title: '정 세 종',
+    small: 'Sejong Jeong',
   },
   contact: [
     {
-      title: 'foo@bar.xyz',
-      link: '#',
+      title: 'sejongjeong@kakao.com',
+      link: 'mailto:sejongjeong@kakao.com',
       icon: faEnvelope,
     },
     {
@@ -25,23 +25,23 @@ const profile: IProfile.Payload = {
       badge: true,
     },
     {
-      title: 'https://github.com',
-      link: 'https://github.com/uyu423/resume-nextjs',
+      title: 'github.com/sejongjeong',
+      link: 'https://github.com/sejongjeong',
       icon: faGithub,
     },
     {
-      link: 'https://www.facebook.com/iu.loen',
+      link: 'https://fb.com/sejong.jeong.kr',
       icon: faFacebook,
     },
     {
-      title: 'YouTube',
-      link: 'https://www.youtube.com/channel/UC3SyT4_WLHzN7JmHQwKQZww',
+      title: 'Curiosity in SJ',
+      link: 'https://curiosityinsj.tistory.com',
       // icon: faRss,
-      icon: faYoutube,
+      icon: faBold,
     },
   ],
   notice: {
-    title: "This resume is a sample page of 'https://github.com/uyu423/resume-nextjs'.",
+    title: "이메일로 연락 부탁드립니다.",
     icon: faBell,
   },
 };
